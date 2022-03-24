63ff99d6c54535e96df4ebc033808c87238b3543
hnipun <Gdhnw1991625%>
2022-03-24 21:26:17 +0530

    added comments

--- gpt/multi_head_attention_layer.py
+++ gpt/multi_head_attention_layer.py
@@ -26,11 +26,13 @@ class MultiHeadAttentionLayer(nn.Module):
 
     def forward(self, x):
         seq_length = x.shape[1]
-
+        # x -> (batch_size, seq_length, embedding_dim)
         x_norm = self.norm_layer(x)  # pre-norm layer
-
+        # x_norm -> (batch_size, seq_length, embedding_dim)
         kx = self.key_linear(x_norm)
+        # kx -> (batch_size, seq_length, num_attention_heads * num_attention_features)
         kx = kx.view(BATCH_SIZE, seq_length, NUM_ATTENTION_HEADS, NUM_ATTENTION_FEATURES)
+        # kx -> (batch_size, seq_length, num_attention_heads, num_attention_features)
 
         qx = self.query_linear(x_norm)
         qx = qx.view(BATCH_SIZE, seq_length, NUM_ATTENTION_HEADS, NUM_ATTENTION_FEATURES)
@@ -39,21 +41,28 @@ class MultiHeadAttentionLayer(nn.Module):
         vx = vx.view(BATCH_SIZE, seq_length, NUM_ATTENTION_HEADS, NUM_ATTENTION_FEATURES)
 
         score = torch.einsum('bihd,bjhd ->bijh', qx, kx)
+        # score -> (batch_size, seq_length, seq_length, num_attention_heads)
         score = score / (NUM_ATTENTION_FEATURES ** 0.5)
 
         mask = self._get_mask(seq_length).to(x.device)
+        # mask -> (1, seq_length, seq_length, 1)
         score = score.masked_fill(mask, float("-inf"))
+        # score -> (batch_size, seq_length, seq_length, num_attention_heads)
 
         probs = self.softmax(score)
+        # score -> (batch_size, seq_length, seq_length, num_attention_heads)
 
         output = torch.einsum('bijh,bjhd ->bihd', probs, vx)
+        # output -> (batch_size, seq_length, num_attention_heads, num_attention_features)
         output = output.reshape(BATCH_SIZE, seq_length, NUM_ATTENTION_HEADS * NUM_ATTENTION_FEATURES)
+        # output -> (batch_size, seq_length, num_attention_heads * num_attention_features)
         output = self.linear_layer(output)
+        # output -> (batch_size, seq_length, embedding_dim)
 
         return output + x
 
     @staticmethod
-    def _get_mask(seq_length: int):  # should be batch_size, seq_length, seq_length, heads
+    def _get_mask(seq_length: int):  # should be batch_size, seq_length, seq_length, num_attention_heads
         # 1, seq_length, seq_length, 1 is enough
 
         res = torch.ones(seq_length, seq_length, dtype=bool)
